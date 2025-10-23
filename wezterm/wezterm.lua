9c4dd212b0b0f7bfc6477b6ab5a8ffc74f9f2f1b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-22 19:38:51 -0600

    chore: update `wezterm.lua`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,20 @@
----Pull in the wezterm API
----@type Wezterm
-local wezterm = require("wezterm")
+local wezterm = require("wezterm") ---@type Wezterm
 local mux = wezterm.mux
 local act = wezterm.action
+
+---@param str string
+local function carriage_return(str) return ("%s\r"):format(str) end
 
 ---@param fg_procc_name string
 ---@return boolean
 local function is_shell(fg_procc_name)
-    local shell_names =  {
-        "bash",
-        "zsh",
-        "fish",
-        "sh",
-        "ksh",
-        "dash",
-    }
-
-    ---@type string
-    local process = fg_procc_name:match("[^/\\]+$") or fg_procc_name
+    local shell_names = { "bash", "zsh", "fish", "sh", "ksh", "dash" }
+    local process = fg_procc_name:match("[^/\\]+$") or fg_procc_name ---@type string
     for _, shell in ipairs(shell_names) do
         if process == shell then
             return true
         end
     end
-
     return false
 end
 
@@ -45,8 +36,6 @@ wezterm.on("format-tab-title", function(tab)
 end)
 
 wezterm.on("open-uri", function(_, pane, uri)
-    local editor = "nvim"
-
     if uri:find("^file:") ~= 1 or pane:is_alt_screen_active() then
         return
     end
@@ -55,24 +44,25 @@ wezterm.on("open-uri", function(_, pane, uri)
     ---`file://[HOSTNAME]/PATH[#linenr]`
     ---Also the pane is not in an alternate screen (an editor, less, etc)
     local url = wezterm.url.parse(uri)
-
     if not url then
         return
     end
 
+    local editor = "nvim"
     if not is_shell(pane:get_foreground_process_name()) then
         -- No shell detected, we're probably connected with SSH, use fallback command
-        local edit_cmd = (url.fragment ~= nil and (editor .. " +" .. url.fragment) or editor)
-        .. ' "$_f"'
-
+        local edit_cmd = ('%s "$_f"'):format(
+            url.fragment ~= nil and (("%s +%s"):format(editor, url.fragment)) or editor
+        )
         local cmd = '_f="'
-        .. url.file_path
-        .. '"; { test -d "$_f" && { cd "$_f" ; ls -a -p --hyperlink --group-directories-first; }; } '
-        .. '|| { test "$(file --brief --mime-type "$_f" | cut -d/ -f1 || true)" = "text" && '
-        .. edit_cmd
-        .. "; }; echo"
+            .. url.file_path
+            .. '";'
+            .. ' { test -d "$_f" && { cd "$_f" ; ls -Ap --hyperlink --group-directories-first; }; }'
+            .. ' || { test "$(file --brief --mime-type "$_f" | cut -d/ -f1 || true)" = "text" && '
+            .. edit_cmd
+            .. "; }; echo"
 
-        pane:send_text(cmd .. "\r")
+        pane:send_text(carriage_return(cmd))
         return false
     end
 
@@ -84,30 +74,28 @@ wezterm.on("open-uri", function(_, pane, uri)
         "--mime-type",
         url.file_path,
     })
-
     if not success then
         return
     end
 
     if stdout:find("directory") then
-        pane:send_text(wezterm.shell_join_args({ "cd", url.file_path }) .. "\r")
-        pane:send_text(wezterm.shell_join_args({"ls", "-A", "-p", "--group-directories-first"})
-            .. "\r")
-
+        pane:send_text(carriage_return(wezterm.shell_join_args({ "cd", url.file_path })))
+        pane:send_text(
+            carriage_return(
+                wezterm.shell_join_args({ "ls", "-A", "-p", "--group-directories-first" })
+            )
+        )
         return false
     end
-
     if stdout:find("text") then
-        local text = not url.fragment
-            and wezterm.shell_join_args({ editor, url.file_path })
-            or wezterm.shell_join_args({editor, "+" .. url.fragment, url.file_path })
-            .. "\r"
+        local text = not url.fragment and wezterm.shell_join_args({ editor, url.file_path })
+            or carriage_return(
+                wezterm.shell_join_args({ editor, "+" .. url.fragment, url.file_path })
+            )
 
         pane:send_text(text)
         return false
     end
-
-    -- without a return value, we allow default actions
 end)
 
 ---Create a default window but makes it maximize on startup
@@ -118,12 +106,8 @@ end)
 
 ---Maximize all displayed windows on startup
 wezterm.on("gui-attached", function(_)
-    local workspace = mux.get_active_workspace()
-
     for _, window in ipairs(mux.all_windows()) do
-        if window:get_workspace() == workspace then
-            window:gui_window():maximize()
-        end
+        window:gui_window():maximize()
     end
 end)
 
@@ -132,16 +116,16 @@ wezterm.on("window-focus-changed", function(window, _)
     wezterm.log_info(("the focus state of %s changed to %s"):format(id, focused))
 end)
 
-wezterm.on("window-config-reloaded", function(window, _)
-    wezterm.log_info(("the config was reloaded for window %s"):format(window:window_id()))
-end)
+wezterm.on(
+    "window-config-reloaded",
+    function(window, _)
+        wezterm.log_info(("the config was reloaded for window %s"):format(window:window_id()))
+    end
+)
 
 local config = wezterm.config_builder()
 
-config.launch_menu = {
-    { args = { "top" } },
-    { label = "Bash", args = { "bash", "-l" } },
-}
+config.launch_menu = { { args = { "top" } }, { label = "Bash", args = { "bash", "-l" } } }
 
 config.window_background_opacity = 1.0
 config.kde_window_background_blur = true
@@ -151,28 +135,22 @@ config.webgpu_power_preference = "HighPerformance"
 config.webgpu_force_fallback_adapter = false
 
 config.mouse_bindings = {
-    -- Scrolling up while holding CTRL increases the font size
-    {
+    { -- Scrolling up while holding CTRL increases the font size
         event = { Down = { streak = 1, button = { WheelUp = 1 } } },
         mods = "CTRL",
         action = act.IncreaseFontSize,
     },
-
-    -- Scrolling down while holding CTRL decreases the font size
-    {
+    { -- Scrolling down while holding CTRL decreases the font size
         event = { Down = { streak = 1, button = { WheelDown = 1 } } },
         mods = "CTRL",
         action = act.DecreaseFontSize,
     },
-
     {
         event = { Up = { streak = 1, button = "Left" } },
         mods = "CTRL",
         action = act.OpenLinkAtMouseCursor,
     },
-
-    -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-    {
+    { -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
         event = { Down = { streak = 1, button = "Left" } },
         mods = "CTRL",
         action = act.Nop,
@@ -181,18 +159,14 @@ config.mouse_bindings = {
 
 config.hide_mouse_cursor_when_typing = false
 config.mouse_wheel_scrolls_tabs = false
-
 config.launcher_alphabet = "1234567890abcdefghilmnopqrstuvwxyz"
 config.quick_select_alphabet = "aoeuqjkxpyhtnsgcrlmwvzfidb"
-
 config.font_size = 20
 config.font = wezterm.font({
     family = "FiraCode Nerd Font Mono",
-
     stretch = "Normal",
     style = "Normal",
     weight = "Regular",
-
     harfbuzz_features = {
         "calt=1",
         "clig=1",
@@ -246,10 +220,8 @@ config.font = wezterm.font({
 
 config.use_dead_keys = false
 config.use_fancy_tab_bar = true
-
 config.webgpu_force_fallback_adapter = false
 config.warn_about_missing_glyphs = false
-
 config.audible_bell = "Disabled"
 config.scrollback_lines = 64500
 config.exit_behavior = "Close"
@@ -258,21 +230,19 @@ config.cursor_blink_rate = 500
 config.cursor_blink_ease_in = "Linear"
 config.cursor_blink_ease_out = "Linear"
 config.default_cursor_style = "BlinkingBlock"
-
 config.color_scheme = "Tokyo Night Storm (Gogh)"
-
 config.enable_kitty_graphics = true
 config.enable_scroll_bar = false
 config.enable_wayland = true
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_title_reporting = true
-
 config.adjust_window_size_when_changing_font_size = true
 config.automatically_reload_config = true
 config.anti_alias_custom_block_glyphs = true
 config.bold_brightens_ansi_colors = "BrightAndBold"
 config.detect_password_input = true
 config.exit_behavior = "Close"
+config.check_for_updates = false
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
@@ -292,8 +262,5 @@ table.insert(config.hyperlink_rules, {
     format = "https://www.github.com/$1/$3",
 })
 
-config.check_for_updates = false
-
 return config
-
----vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:ft=lua:
+-- vim:ts=4:sts=4:sw=4:et:
